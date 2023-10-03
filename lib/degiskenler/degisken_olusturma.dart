7ff2828251f01d80f52bac308b1f63810d0f99c5
Atilla Burak YÜKSEL <atillaburakyuksel@Atilla-MacBook-Pro.local>
2023-10-03 23:02:36 +0300

    1.gün eğitimi.

---
+++ lib/degiskenler/degisken_olusturma.dart
@@ -0,0 +1,34 @@
+void main()
+{
+ /* var ogrenciAdi="atilla";
+  var ogrenciYas=23;
+  var ogrenciBoy=1.78;
+  var ogrenciBasHarf="A";//'A'
+  var ogrenciDevamEdiyorMu=true;
+
+  print(ogrenciAdi);
+  print(ogrenciYas);
+  print(ogrenciBoy);
+  print(ogrenciBasHarf);
+  print(ogrenciDevamEdiyorMu); */
+  
+ /* int urun_id=3416;
+  String urun_adi="Kol saati";
+  int urun_adet=100;
+  double urun_fiyat=149.99;
+  String urun_tedarikci="rolex";
+  print("ÜRÜN BİLGİLERİ");
+  print("Ürün id         : $urun_id");
+  print("Ürün adı        : $urun_adi");
+  print("Ürün adet       : $urun_adet");
+  print("Ürün fiyatı     : $urun_fiyat ₺");
+  print("Ürün tedarikçisi: $urun_tedarikci");
+  */
+
+  int a=100;
+  int b=200;
+  print("$a ve $b 'nin toplamı ${a+b}");
+  print("$a ve $b 'nin çarpımı ${a*b}");
+  print("$a ve $b 'nin bölümü ${a/b}");
+  print("$a ve $b 'nin farkı ${a-b}");
+}
---
+++ lib/degiskenler/sabitler.dart
@@ -0,0 +1,20 @@
+void main(){
+  //sabitler
+  //final,const,java(final),swift(let),kotlin(val)
+  int sayi = 30;
+  print(sayi);
+  sayi = 100;
+  print(sayi);
+
+  //kodlamayı çalıştırdığımızda hafızada oluşur.
+  final int numara = 10;
+  print(numara);
+  //numara = 200;
+
+  //değişkeni tanımladığımızda hafızada oluşur.
+  //görsel nesnelerde kullanılır.button,text vb.
+  const int sonuc = 100;
+  print(sonuc);
+  //numara 200;
+
+}
